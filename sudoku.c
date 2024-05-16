6ef1e4cfa39262d9dba538d4aee4a6ea13f28aa9
Your Name <you@example.com>
2024-05-16 02:52:34 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -49,8 +49,8 @@ int is_valid(Node* n){
       aux_array[i] = 0;
   for(int i = 0; i < 9; i++)
     {
-      printf("%i ", aux_array[n->sudo[i][0]]);
-      printf("%ij ", n->sudo[i][0]):
+      printf("%i \n", aux_array[n->sudo[i][0]]);
+      printf("%ij \n", n->sudo[i][0]);
       if(aux_array[n->sudo[i][0]] == 0)
           aux_array[n->sudo[i][0]] = 1;
       else if(aux_array[n->sudo[i][0]] == 1)

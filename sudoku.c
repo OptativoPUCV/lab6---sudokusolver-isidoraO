edb3f7d23c30b47e916c9627605e7700dc36b454
Your Name <you@example.com>
2024-05-16 03:46:17 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -68,6 +68,7 @@ int is_valid(Node* n){
         for(p=0;p<9;p++){
             int k=3*(j/3) + (p/3) ;
             int l=3*(j%3) + (p%3) ;
+            printf("%i ", n->sudo[k][l]);
             if(aux_array_submatrix[n->sudo[k][l] == 0])
               aux_array_submatrix[n->sudo[k][l]] = 1;
             else if(aux_array_submatrix[n->sudo[k][l]] == 1)

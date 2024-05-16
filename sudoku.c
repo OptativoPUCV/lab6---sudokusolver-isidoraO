7f7042d40b98a4bf309ee99ca77ec9023e77d096
Your Name <you@example.com>
2024-05-16 04:47:43 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -71,15 +71,7 @@ int is_valid(Node* n){
         else if(aux_array_col[n->sudo[j][i]] == 1)
             return 0;
       }
-      for(k = 0; k < 9; k++)
-        {
-          int l = 3 * (j / 3) + (k / 3);
-          int p = 3 * (j % 3) + (k % 3);
-          if((n->sudo[l][p] != 0) && (aux_array_submatriz[n->sudo[l][p]] == 0))
-              aux_array_submatriz[n->sudo[l][p]] = 1;
-          else if(aux_array_submatriz[n->sudo[l][p]] == 1)
-              return 0;
-        }
+
   }
   return 1;
 }
@@ -106,6 +98,7 @@ List* get_adj_nodes(Node* n)
                 if(cont == 9)  
                   cont = 0;
               }
+          printf("%i ", is_valid(nodo));
           if(nodo != NULL)
             pushBack(list, nodo);
           cont++;

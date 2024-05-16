0a178b3e94e0b32eaa9af5ed516495951c651db1
Your Name <you@example.com>
2024-05-16 04:04:52 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -65,7 +65,6 @@ int is_valid(Node* n){
           aux_array_col[n->sudo[j][i]] = 1;
         else if(aux_array_col[n->sudo[j][i]] == 1)
             return 0;
-        for(p=0;p<9;p++){
             int k=3*(j/3) + (i/3) ;
             int l=3*(j%3) + (i%3) ;
            printf("pp%i ", n->sudo[k][l]);
@@ -80,7 +79,7 @@ int is_valid(Node* n){
                 printf("d%i ",n->sudo[k][l]);
                return 0;
               }
-        }
+        
       }
     }
   return 1;

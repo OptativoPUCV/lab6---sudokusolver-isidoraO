4c5e22494528225beb484e5fbca3566a83ff791a
Your Name <you@example.com>
2024-05-18 02:06:29 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -135,7 +135,7 @@ Node* DFS(Node* initial, int* cont){
     {
       Node *n = top(stack);
       pop(stack);
-      if(is_final(n))
+      if(is_final(n) == 0)
         return n;
       adj_nodes = get_adj_nodes(n);
       Node *aux = first(adj_nodes);

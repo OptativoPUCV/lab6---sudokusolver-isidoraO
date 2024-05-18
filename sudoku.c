a2671607ffa8e522639e96c87df8213099a09c9d
Your Name <you@example.com>
2024-05-18 18:16:25 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -94,6 +94,7 @@ List* get_adj_nodes(Node* n)
       {
         for(int i = 0; i < 9; i++)
         {
+          printf("%i ", i);
           for(int j = 0; j < 9 ; j++)
             {
               if(n->sudo[i][j] == 0)
